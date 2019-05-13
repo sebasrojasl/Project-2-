8d42d0b4e52c09f5bd722b74ab79c83d359ca84d
sebasrojasl <42528969+sebasrojasl@users.noreply.github.com>
2019-05-13 12:11:37 -0600

    Delete user.sql

--- Tablas/user.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `project2`.`user` (
-  `id_user` INT(11) NOT NULL AUTO_INCREMENT COMMENT 'Unique identifier ',
-  `user_name` VARCHAR(50) NOT NULL COMMENT 'name of the user',
-  `password` VARCHAR(50) NOT NULL COMMENT 'password of the user',
-  `id_user_type` INT(11) NOT NULL COMMENT 'reference to the type of user',
-  PRIMARY KEY (`id_user`),
-  UNIQUE INDEX `user_name_UNIQUE` (`user_name` ASC) VISIBLE)
-COMMENT = 'Table that contains the user.';
