1289a71dff0d42f5720535402f33df59c5888617
Alejandra Rivera <48934886+alerivera0901@users.noreply.github.com>
2019-05-13 13:41:30 -0600

    Update award.sql

--- Tablas/award.sql
+++ Tablas/award.sql
@@ -1,13 +1,20 @@
 CREATE TABLE `project2`.`award` (
-  `id_award` INT(11) NOT NULL AUTO_INCREMENT COMMENT 'Identifier of the award.',
-  `award_name` VARCHAR(50) NOT NULL COMMENT 'name of the award',
-  `year` INT(11) NOT NULL,
-  `id_award_type` INT(11) NOT NULL,
+  `id_award` INT NOT NULL AUTO_INCREMENT COMMENT 'unique identifier of th award',
+  `award_name` VARCHAR(45) NOT NULL COMMENT 'the name of the award',
+  `year` INT NOT NULL COMMENT 'year of the award',
+  `id_team` INT NOT NULL,
+  `id_award_type` INT NOT NULL,
   PRIMARY KEY (`id_award`),
-  INDEX `id_award_fk_idx` (`id_award_type` ASC) VISIBLE,
-  CONSTRAINT `id_award_fk`
+  INDEX `id_award_type_fk_idx` (`id_award_type` ASC) VISIBLE,
+  INDEX `id_team_fk_idx` (`id_team` ASC) VISIBLE,
+  CONSTRAINT `id_award_type_fk`
     FOREIGN KEY (`id_award_type`)
     REFERENCES `project2`.`award_type` (`id_award_type`)
     ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `id_team_fk`
+    FOREIGN KEY (`id_team`)
+    REFERENCES `project2`.`team` (`id_team`)
+    ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-COMMENT = 'Table that storage all the awards';
+COMMENT = 'table that storages awards';
