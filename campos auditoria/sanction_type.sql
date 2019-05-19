c4cbc9c46475c743a8ce622a45f1d59becfa15e8
sebasrojasl <42528969+sebasrojasl@users.noreply.github.com>
2019-05-19 12:08:48 -0600

    Delete sanction_type.sql

--- campos auditoria/sanction_type.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `project2`.`sanction_type` 
-ADD COLUMN `modification_date` DATE NULL DEFAULT NULL AFTER `sanction_name`,
-ADD COLUMN `modification_user` VARCHAR(45) NULL DEFAULT NULL AFTER `modification_date`;
